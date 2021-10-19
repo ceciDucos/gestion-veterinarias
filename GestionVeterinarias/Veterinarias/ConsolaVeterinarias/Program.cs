3cb91066074a81dc5c3fe2059facd602546af753
Gonzalo Cáceres <caceresg301@gmail.com>
2021-10-18 21:31:34 -0300

    Va de nuevo
    Va de nuevo

---
+++ GestionVeterinarias/Veterinarias/ConsolaVeterinarias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolaVeterinarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
