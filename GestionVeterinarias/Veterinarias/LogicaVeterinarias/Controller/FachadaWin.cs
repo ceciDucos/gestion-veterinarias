3364fd84b1d38d32a22df2cf0b39117cf9dc1953
Gonzalo Cáceres <caceresg301@gmail.com>
2021-10-26 22:47:28 -0300

    Nuevas tareas repartidas (FachadaWin)

--- GestionVeterinarias/Veterinarias/LogicaVeterinarias/Controller/FachadaWin.cs
+++ GestionVeterinarias/Veterinarias/LogicaVeterinarias/Controller/FachadaWin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogicaVeterinarias.ValueObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,72 @@ namespace LogicaVeterinarias.Controller
 {
     class FachadaWin
     {
+        public void CrearVeterianaria(VOVeterinaria voveterinaria)
+        {
+        }
+
+        public void EditarVeterianaria(VOVeterinaria voveterinaria)
+        {
+        }
+
+        public void EliminarVeterianaria(int num)
+        {
+        }
+
+        public void CrearVeterianario(VOVeterinario voveterinario)
+        {
+        }
+
+        public void  EditarVeterianario(VOVeterinario voveterinario)
+        {
+        }
+
+        public void  EliminarVeterianario(long cedula)
+        {
+        }
+
+        public void  CrearCliente(VOCliente vocliente)
+        {
+        }
+
+        public void  EditarCliente(VOCliente vocliente)
+        {
+        }
+
+        public void  EliminarCliente(long cedula)
+        {
+        }
+
+        public void  CrearMascota(VOMascota vomascota)
+        {
+        }
+
+        public void  EditarMascota(VOMascota vomascota)
+        {
+        }
+
+        public void   EliminarMascota(int num)
+        {
+        }
+
+        public void  CrearCarne(VOCarnetInscripcion vocarnet)
+        {
+        }
+
+        public void  EditarCarne(VOCarnetInscripcion vocarnet)
+        {
+        }
+
+        public void  CrearConsulta(VOConsulta voconsulta)
+        {
+        }
+
+        public void  EditarConsulta(VOConsulta voconsulta)
+        {
+        }
+
+        public void  EliminarConsulta(int num)
+        {
+        }
     }
 }
