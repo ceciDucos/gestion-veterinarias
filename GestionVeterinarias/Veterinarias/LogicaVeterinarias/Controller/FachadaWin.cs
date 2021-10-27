f4244a6b898cc6481627c60d68e82e3992483113
Gonzalo Cáceres <caceresg301@gmail.com>
2021-10-26 23:27:59 -0300

    Repartido de fachada

--- GestionVeterinarias/Veterinarias/LogicaVeterinarias/Controller/FachadaWin.cs
+++ GestionVeterinarias/Veterinarias/LogicaVeterinarias/Controller/FachadaWin.cs
@@ -4,75 +4,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Services;
 
 namespace LogicaVeterinarias.Controller
 {
-    class FachadaWin
+    class FachadaWin : System.Web.Services.WebService
     {
+        [WebMethod]
         public void CrearVeterianaria(VOVeterinaria voveterinaria)
         {
         }
 
+        [WebMethod]
         public void EditarVeterianaria(VOVeterinaria voveterinaria)
         {
         }
 
+        [WebMethod]
         public void EliminarVeterianaria(int num)
         {
         }
 
+        [WebMethod]
         public void CrearVeterianario(VOVeterinario voveterinario)
         {
         }
 
+        [WebMethod]
         public void  EditarVeterianario(VOVeterinario voveterinario)
         {
         }
 
+        [WebMethod]
         public void  EliminarVeterianario(long cedula)
         {
         }
 
+        [WebMethod]
         public void  CrearCliente(VOCliente vocliente)
         {
         }
 
+        [WebMethod]
         public void  EditarCliente(VOCliente vocliente)
         {
         }
 
+        [WebMethod]
         public void  EliminarCliente(long cedula)
         {
         }
 
+        [WebMethod]
         public void  CrearMascota(VOMascota vomascota)
         {
         }
 
+        [WebMethod]
         public void  EditarMascota(VOMascota vomascota)
         {
         }
 
+        [WebMethod]
         public void   EliminarMascota(int num)
         {
         }
 
+        [WebMethod]
         public void  CrearCarne(VOCarnetInscripcion vocarnet)
         {
         }
 
+        [WebMethod]
         public void  EditarCarne(VOCarnetInscripcion vocarnet)
         {
         }
 
+        [WebMethod]
         public void  CrearConsulta(VOConsulta voconsulta)
         {
         }
 
+        [WebMethod]
         public void  EditarConsulta(VOConsulta voconsulta)
         {
         }
 
+        [WebMethod]
         public void  EliminarConsulta(int num)
         {
         }
