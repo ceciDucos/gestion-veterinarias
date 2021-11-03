37e9661a0f34889d905746190991e1a2e7266388
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2021-11-03 13:31:49 -0300

    comments

--- GestionVeterinarias/Veterinarias/ConsolaVeterinarias/Program.cs
+++ GestionVeterinarias/Veterinarias/ConsolaVeterinarias/Program.cs
@@ -11,9 +11,20 @@ namespace ConsolaVeterinarias
         static void Main(string[] args)
         {
             FachadaWin fachadaWin = new FachadaWin();
-            //Pruebas Ceci
+            // ================== INICIO PRUEBAS CECI ==================
 
-<<<<<<< HEAD
+            //fachadaWin.CrearCliente(new VOCliente(11111789, "CeciNuevo", "099000999", "addressceci", "ceci@gmail.com", "pass", true));
+            //fachadaWin.EditarCliente(new VOCliente(11111789, "testingEdit", "123123123", "editAdress", "edit@gmail.com", "1234pass", false));
+            //fachadaWin.EliminarCliente(11111789);
+
+            //byte[] foto = null;
+            //VOMascota voMascota = new VOMascota(999, TipoAnimal.Perro, "Larry", Raza.Golden, 4, true, new VOCarnetInscripcion(new DateTime(), foto));
+            //DateTime date = DateTime.Today;
+            //fachadaWin.CrearConsulta(new VOConsulta(0, date, "algo", 10, voMascota));
+            //fachadaWin.EliminarConsulta(1);
+            //fachadaWin.EditarConsulta(new VOConsulta(1, date, "yuyuyuyuyu", 9, voMascota));
+            // ================== FIN PRUEBAS CECI ==================
+
             // ================== INICIO FEDE PRUEBAS ==================
 
             //fachadaWin.CrearVeterianaria(new VOVeterinaria("Los gatitos", "nenecho", "091234567"));
@@ -38,9 +49,9 @@ namespace ConsolaVeterinarias
             //fachadaWin.EditarCarnet(new VOCarnetInscripcion(10000, DateTime.Now, arr2));
             //Console.ReadLine();
 
-           // ================== FIN FEDE PRUEBAS ==================
+            // ================== FIN FEDE PRUEBAS ==================
 
-             // ================== INICIO RODRIGO PRUEBAS ==================
+            // ================== INICIO RODRIGO PRUEBAS ==================
             /*
             try{
                 fachadaWin.CrearVeterinario(new VOVeterinario(123456, "Juan", "12345", "88888"));
@@ -56,36 +67,25 @@ namespace ConsolaVeterinarias
 
              }
             */
-=======
-            //fachadaWin.CrearCliente(new VOCliente(11111789, "CeciNuevo", "099000999", "addressceci", "ceci@gmail.com", "pass", true));
-            //fachadaWin.EditarCliente(new VOCliente(11111789, "testingEdit", "123123123", "editAdress", "edit@gmail.com", "1234pass", false));
-            //fachadaWin.EliminarCliente(11111789);
 
-            //byte[] foto = null;
-            //VOMascota voMascota = new VOMascota(999, TipoAnimal.Perro, "Larry", Raza.Golden, 4, true, new VOCarnetInscripcion(new DateTime(), foto));
-            //DateTime date = DateTime.Today;
-            //fachadaWin.CrearConsulta(new VOConsulta(0, date, "algo", 10, voMascota));
-            //fachadaWin.EliminarConsulta(1);
-            //fachadaWin.EditarConsulta(new VOConsulta(1, date, "yuyuyuyuyu", 9, voMascota));
- 
             // ================== INICIO RODRIGO PRUEBAS ==================
-             /* 
-             try{
-                 fachadaWin.CrearVeterinario(new VOVeterinario(123456, "Juan", "12345", "88888"));
-                 fachadaWin.CrearVeterinario(new VOVeterinario(5678, "Pepito", "12345", "88888"));
-                 fachadaWin.EliminarVeterinario(123456);
-                 VOVeterinario vo_para_editar = new VOVeterinario(5678, "qqqq", "5555", "Lunes a Dom de 10a21");
-                 fachadaWin.EditarVeterinario(vo_para_editar);
-                 Console.WriteLine("Ejecutada con exito");
-              }
-              catch (Exception e)
-              {
-                 Console.WriteLine("Error: {0} ", e.Message);
+            /* 
+            try{
+                fachadaWin.CrearVeterinario(new VOVeterinario(123456, "Juan", "12345", "88888"));
+                fachadaWin.CrearVeterinario(new VOVeterinario(5678, "Pepito", "12345", "88888"));
+                fachadaWin.EliminarVeterinario(123456);
+                VOVeterinario vo_para_editar = new VOVeterinario(5678, "qqqq", "5555", "Lunes a Dom de 10a21");
+                fachadaWin.EditarVeterinario(vo_para_editar);
+                Console.WriteLine("Ejecutada con exito");
+             }
+             catch (Exception e)
+             {
+                Console.WriteLine("Error: {0} ", e.Message);
 
-              }
-             */
->>>>>>> 5315b05... cosulta entero y edit cliente
-             Console.ReadLine();
+             }
+            */
+
+            Console.ReadLine();
 
             // ================== FIN RODRIGO PRUEBAS ==================
         }
--- GestionVeterinarias/Veterinarias/ModelosVeterinaria/ValueObject/VOCarnetInscripcion.cs
+++ GestionVeterinarias/Veterinarias/ModelosVeterinaria/ValueObject/VOCarnetInscripcion.cs
@@ -8,7 +8,6 @@ namespace ModelosVeterinarias.ValueObject
 {
     public class VOCarnetInscripcion
     {
-<<<<<<< HEAD
         private int numero;
 
         public int Numero { get { return numero; } }
@@ -16,19 +15,6 @@ namespace ModelosVeterinarias.ValueObject
         public byte[] Foto { get; }
 
         public VOCarnetInscripcion(int numero, DateTime expedido, byte[] foto)
-=======
-        private int num;
-        public int Numero { get { return num; } }
-        public DateTime Expedido { get; }
-        public byte[] Foto { get; }
-
-        public VOCarnetInscripcion(DateTime expedido, byte[] foto)
-        {
-            this.Expedido = expedido;
-            this.Foto = foto;
-        }
-        public VOCarnetInscripcion(int num, DateTime expedido, byte[] foto)
->>>>>>> 5315b05... cosulta entero y edit cliente
         {
             this.numero = numero;
             this.Expedido = expedido;
