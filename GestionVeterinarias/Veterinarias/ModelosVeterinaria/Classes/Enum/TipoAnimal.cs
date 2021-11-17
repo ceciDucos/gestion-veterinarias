3412f45ee5b0311ee7b6d8c8de2dca92e3484a7e
Gonzalo Cáceres <caceresg301@gmail.com>
2021-11-17 20:20:43 -0300

    Agregado de Animales y Razas
    Agregado de Animales y Razas

--- GestionVeterinarias/Veterinarias/ModelosVeterinaria/Classes/Enum/TipoAnimal.cs
+++ GestionVeterinarias/Veterinarias/ModelosVeterinaria/Classes/Enum/TipoAnimal.cs
@@ -1,11 +1,17 @@
 
 public enum TipoAnimal
 {
-    Gato = 0,
-    Perro = 1,
-    Loro = 2,
-    Huron = 3,
+    Otro = 1,
+    Gato = 2,
+    Perro = 3,
     Conejo = 4,
     Cobayo = 5,
-    Hamster = 6
+    Hamster = 6,
+    Tortuga = 7,
+    Chinchilla = 8,
+    Pajaro = 9,
+    Cerdo = 10,
+    Pez = 11,
+    Loro = 12,
+    Huron = 13,
 }
