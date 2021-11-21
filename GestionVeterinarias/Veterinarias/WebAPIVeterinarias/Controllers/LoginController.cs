f6aeba090cfa1f475caf8e542d875823c284e9e3
fp90 <54083943+fp90@users.noreply.github.com>
2021-11-20 22:44:42 -0300

    fix cors for login (#51)

--- GestionVeterinarias/Veterinarias/WebAPIVeterinarias/Controllers/LoginController.cs
+++ GestionVeterinarias/Veterinarias/WebAPIVeterinarias/Controllers/LoginController.cs
@@ -7,9 +7,11 @@ using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPIVeterinarias.Controllers
 {
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     [AllowAnonymous]
     [RoutePrefix("api/login")]
     public class LoginController : ApiController
